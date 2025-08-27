2aa1afdc99865833b806e49cc639b619cdcfaacb
Alina Vannarath <alinavannarath@gmail.com>
2025-08-26 19:57:37 -0600

    Updated n = 100 to n = 150

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -4,7 +4,7 @@
 # 2025-08-26
 
 # Simulate predictor variable
-predictor <- rnorm(n = 100)
+predictor <- rnorm(n = 150)
 # Simulate response variable with some noise
 response <- 2 * predictor + rnorm(n = 100, sd = 0.2)
 
