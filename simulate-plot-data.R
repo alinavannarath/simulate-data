bb3b9caf5b2804668faf79e88b976eb05d8176d0
Alina Vannarath <alinavannarath@gmail.com>
2025-08-26 19:37:01 -0600

    Add files via upload

---
+++ simulate-plot-data.R
@@ -0,0 +1,12 @@
+# Simulate and plot data
+# Alina Vannarath
+# u1258689@utah.edu 
+# 2025-08-26
+
+# Simulate predictor variable
+predictor <- rnorm(n = 100)
+# Simulate response variable with some noise
+response <- 2 * predictor + rnorm(n = 100, sd = 0.2)
+
+# Plot the data
+plot(x = predictor, y = response)
